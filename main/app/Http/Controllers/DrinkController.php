99317a791f9e958f0b1007cfb48a3f88e4a618e0
AdrianaMargiotta <adrianamargiotta@protonmail.com>
2021-02-02 17:42:24 +0100

    step 1

--- main/app/Http/Controllers/DrinkController.php
+++ main/app/Http/Controllers/DrinkController.php
@@ -7,26 +7,24 @@ use Illuminate\Http\Request;
 
 class DrinkController extends Controller
 {
-    public function index()
-  {
-    $drinks = Drink::all();
-    return view ('pages.drinks', compact('drinks'));
-  }
+    public function index(){
+        $drinks = Drink::all();
+        return view ('pages.drinks', compact('drinks'));
+    }
 
-  public function show($id)
-  {
-    $drink = Drink::findOrFail($id);
-    return view ('pages.drinks-show', compact('drink'));
-  }
+    public function show($id){
+        $drink = Drink::findOrFail($id);
+        return view ('pages.drinks-show', compact('drink'));
+    }
 
-  public function create()
-  {
-    return view ('pages.drinks-create');
-  }
+    public function create(){
+        return view ('pages.drinks-create');
+    }
+    public function store(Request $request){
+        Drink::create($request -> all());
+        return redirect() -> route('home-drinks');
+    }
 
-  public function store(Request $request)
-  {
-    Drink::create($request -> all());
-    return redirect() -> route('home-drinks');
-  }
+    
+
 }
